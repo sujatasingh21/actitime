37a8f927065de66e01b73dfcaafcf9d3c2fb8dc9
sujatakr <sujatakumari2821@gmail.com>
2022-12-21 09:01:18 +0530

    added an interface

---
+++ src/test/java/generic/IAutoConst.java
@@ -0,0 +1,8 @@
+package generic;
+
+public interface IAutoConst {
+	
+	public static final  String PPT_PATH="base.properties";
+	public static final  String REPORT_PATH="./target/Spark.html";
+	public static final  String XL_PATH="./data/input.xlsx";
+}
